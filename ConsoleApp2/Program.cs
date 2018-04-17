2a6793c739e9e2b11cba3fdd969478149a7ee038
Ruslan Hassonov <ruslan.hassonov@gmail.com>
2018-04-17 23:15:13 +0200

    First Commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ones = new string[] { "zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tens = new string[] { "zero","ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+            int number = 0;
+            string words = string.Empty;
+
+            do
+            {
+                Console.Write("Input: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number < 20 )
+                {
+                    words = ones[number];
+                    Console.WriteLine("Output: " + words);
+                }
+                else if (number % 10 == 0)
+                {
+                    words = tens[number/10];
+                    Console.WriteLine("Output: " + words);
+                }
+                else
+                {
+                    words = tens[number / 10] +" "+ones[number % 10];
+                    Console.WriteLine("Output: " + words);
+                }
+                
+            } while (number != 999); // Just an input to stop the loop
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Number To Words
+
+A small program I got from  a coding challenge. The program compares the user input with the corresponding array value.
